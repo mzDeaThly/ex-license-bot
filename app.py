804f81504eb150d17c29a0c010da766e9b6e9984
mzDeaThly <162259866+mzDeaThly@users.noreply.github.com>
2025-07-27 22:37:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ VALID_LICENSES = {
     'EX-DEV-888': {
         'expires_on': '2025-12-31',
         'api_key': 'CAP-ECED32012CF8CDCBE211FC698950482F8EE7669B23512943594905547D2E60E1',
-        'session': None
+        'session': None 
     },
     'EX-TEST': {
-        'expires_on': '2024-07-31',
+        'expires_on': '2025-07-31',
         'api_key': 'CAP-ECED32012CF8CDCBE211FC698950482F8EE7669B23512943594905547D2E60E1',
-        'session': None
+        'session': None 
     }
 }
 
